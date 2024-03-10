f28d2f400a825a914134deafb2874ed719825f13
yaseen-khan <yaseenkhan61@yahoo.com>
2024-03-10 18:29:10 +0530

    managing colors globally

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,4 +1,5 @@
 import { Text, View, Pressable, StyleSheet } from 'react-native';
+import Colors from '../constants/colors';
 
 function PrimaryButton({ children, onPress }) {
 
@@ -9,7 +10,7 @@ function PrimaryButton({ children, onPress }) {
                     ? [styles.buttonInnerContainer, styles.pressed]
                     : styles.buttonInnerContainer}
                 onPress={onPress}
-                android_ripple={{ color: '#640233' }}>
+                android_ripple={{ color: Colors.primary600 }}>
                 <Text style={styles.buttonText}>{children}</Text>
             </Pressable >
         </View>
@@ -25,7 +26,7 @@ const styles = StyleSheet.create({
         overflow: 'hidden',
     },
     buttonInnerContainer: {
-        backgroundColor: '#72063c',
+        backgroundColor: Colors.primary500,
         paddingVertical: 8,
         paddingHorizontal: 16,
         elevation: 2
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { TextInput, View, StyleSheet, Alert } from 'react-native'
 
 import PrimaryButton from '../components/PrimaryButton'
+import Colors from '../constants/colors';
 
 function StartGameScreen({ onPickNumber }) {
     const [enteredValue, setEnteredNumber] = useState('');
@@ -61,7 +62,7 @@ const styles = StyleSheet.create({
         marginTop: 100,
         padding: 16,
         marginHorizontal: 24,
-        backgroundColor: '#3b021f',
+        backgroundColor: Colors.primary800,
         borderRadius: 8,
         elevation: 4,
         shadowColor: 'black',
@@ -75,9 +76,9 @@ const styles = StyleSheet.create({
         fontSize: 32,
         fontWeight: 'bold',
         textAlign: 'center',
-        borderBottomColor: '#ddb52f',
+        borderBottomColor: Colors.accent500,
         borderBottomWidth: 2,
-        color: '#ddb52f',
+        color: Colors.accent500,
         marginVertical: 8
     },
     buttonsContainer: {
